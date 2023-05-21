131e5816b2f62d39ff51e0ef650dce6b367f7006
YU000jp <111847207+YU000jp@users.noreply.github.com>
2023-05-21 23:37:11 +0900

    add credit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import '@logseq/libs'; //https://plugins-doc.logseq.com/
 //import { setup as l10nSetup, t } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
 //import ja from "./translations/ja.json";
 
+
+//Credit: ottodevs  https://discuss.logseq.com/t/show-week-day-and-week-number/12685/18
 function addExtendedDate(titleElement: HTMLElement) {
   // check if element already has date info
   const existingSpan = titleElement.querySelector("span");
