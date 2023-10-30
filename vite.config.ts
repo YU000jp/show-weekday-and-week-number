2d9ab210e26440801380e6de9b3e1becd44fc351
YU <111847207+YU000jp@users.noreply.github.com>
2023-10-30 13:13:13 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,12 @@
-import { defineConfig } from 'vite';
-import { string } from 'rollup-plugin-string';
-import logseqDevPlugin from 'vite-plugin-logseq';
+import { defineConfig } from "vite";
+import logseqDevPlugin from "vite-plugin-logseq";
 
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  const forProd = mode === 'production';
-
-  return {
-    plugins: [
-      // Makes HMR available for development
-      logseqDevPlugin(),
-      {
-        ...string({
-          include: '**/*.css',
-        }),
-        enforce: 'pre',
-      },
-    ],
-    build: {
-      sourcemap: !forProd,
-      target: 'esnext',
-      minify: forProd ? 'esbuild' : false,
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [logseqDevPlugin()],
+  // Makes HMR available for development
+  build: {
+    target: "esnext",
+    minify: "esbuild",
+  },
 });
