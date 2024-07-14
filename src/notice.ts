aabc7e11750e409632f39a8b25871fbedaae7974
YU <111847207+YU000jp@users.noreply.github.com>
2024-07-14 10:18:43 +0900

    Update Notice

--- src/notice.ts
+++ src/notice.ts
@@ -8,22 +8,7 @@ export const notice = () => {
                   // マークダウン形式でメッセージを表示する
                   logseq.UI.showMsg(`
   
-      📆"Show weekday and week-number" plugin
-      Updated!
-      
-  
-      Feature:
-      1. Week-number format options
-      2. Monthly Journal (Insert Template)
-      3. Quarterly Journal (Insert Template)
-      4. (Weekly/M/Q) Journal Nav link
-  
-      - New setting items have been added in the plugin settings.
-  
-  
-      Bug fix:
-      1. Show indicator (dot) of journal entries
-        (⚠️Due to changes in the specifications of Logseq app, judgments are made based on the database rather than the file.)
+      [:h3 📆"Show weekday and week-number" plugin Updated!]
       
       `, "info", { timeout: 8500 })
                   logseq.showSettingsUI() // 設定画面を表示する
